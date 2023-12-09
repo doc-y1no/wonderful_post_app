e30cbcf6638d30029076c5228a2238af0e629e8a
Apple <kisin1010@gmail.com>
2023-12-09 17:53:51 +0900

    rails db:reset実行　サーバが立ち上がるように修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_26_171740) do
+ActiveRecord::Schema.define(version: 2023_12_09_052555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,4 +28,5 @@ ActiveRecord::Schema.define(version: 2023_11_26_171740) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
+
 end
